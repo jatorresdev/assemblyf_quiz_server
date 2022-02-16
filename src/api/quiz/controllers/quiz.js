ca4dceba56957b982f11538548fa70106344ecbe
Angel Torres <jatorresdev@gmail.com>
2022-02-15 22:45:51 -0500

    feat: error handling is added

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -13,22 +13,33 @@ module.exports = createCoreController('api::quiz.quiz', ({ strapi }) => ({
         const quiz = await strapi.service('api::quiz.quiz').findOne(id, { populate: { questions: true } });
         let score = 0;
 
-        if (quiz && Object.keys(userAnswers).length !== 0) {
-            userAnswers.map((userAnswer) => {
-                const question = quiz.questions.find((element) => element.id === userAnswer.questionId);
-
-                if (question) {
-                    if (question.answerCorrect === userAnswer.questionAnswer) {
-                        score++;
-                    }
-                }
-            });
+        if (!quiz) {
+            return ctx.notFound()
         }
+
+        if (Object.keys(userAnswers).length === 0) {
+            return ctx.badRequest('The request body is empty', userAnswers)
+        }
+
+        const answers = userAnswers.filter((userAnswer) => {
+            const question = quiz.questions.find((element) => element.id === userAnswer.questionId);
+
+            if (question) {
+                userAnswer.correct = false;
+
+                if (question.answerCorrect === userAnswer.questionAnswer) {
+                    userAnswer.correct = true;
+                    score++;
+                }
+
+                return userAnswer;
+            }
+        });
 
         const questionCount = quiz.questions?.length;
 
         delete quiz.questions;
 
-        return { quiz, score, scoredAnswers: userAnswers, questionCount };
+        return { quiz, answers, score, questionCount };
     },
 }));
